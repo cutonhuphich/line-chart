01b7a4a58b5d2dedeae6b3d3f87119fe42baf6c1
Tran Quang Nam <namtrquang@gmail.com>
2021-10-24 13:39:58 +0700

    update progress

--- app/src/main/java/com/example/linechart/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/linechart/ui/activity/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initAction() {
         buttonStart?.setOnClickListener {
-            circleTemperature?.setValue(20F)
+            circleTemperature?.setValue(15F)
         }
     }
 
--- app/src/main/java/com/example/linechart/ui/view/CircleTemperature.kt
+++ app/src/main/java/com/example/linechart/ui/view/CircleTemperature.kt
@@ -4,37 +4,95 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.linechart.R
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.*
 
 class CircleTemperature : View, ValueAnimator.AnimatorUpdateListener {
     constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(attrs)
+        getValueData()
+    }
 
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    private fun getValueData() {
+        val stepData = (maxValue - minValue).toFloat() / DISPLAY_LEVEL
+        for (i in 0..DISPLAY_LEVEL) {
+            valueData.add(getStringValue(-(minValue + (i * stepData))))
+        }
+    }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
+    private fun init(attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleTemperatureView)
+        radius = typedArray.getInt(R.styleable.CircleTemperatureView_radius, DEFAULT_RADIUS)
+        minValue = typedArray.getInt(R.styleable.CircleTemperatureView_min, DEFAULT_MIN_VALUE)
+        maxValue = typedArray.getInt(R.styleable.CircleTemperatureView_max, DEFAULT_MAX_VALUE)
+        currentValue = minValue.toFloat()
+        typedArray.recycle()
+    }
 
-    private val minValue = -20F
-//     fun setMinValue(value: Float) {
-//        this.minValue = value
-//        invalidate()
-//    }
+    private var rectF = RectF()
+    private var minValue: Int = DEFAULT_MIN_VALUE
+    private var maxValue: Int = DEFAULT_MAX_VALUE
+    private var radius: Int = DEFAULT_RADIUS
+    private var marginContent: Float = MARGIN_CONTENT
+    private var spaceTextAndLine: Float = SPACE_TEXT_AND_LINE
+    private var spaceLineAndProgress: Float = SPACE_LINE_AND_PROGRESS
+    private var spaceProgressAndCircle: Float = SPACE_PROGRESS_AND_CIRCLE
+    private var lengthLineShort: Float = LENGTH_LINE_SHORT
+    private var lengthLineLong: Float = LENGTH_LINE_LONG
+    private var textSizeValue: Float = TEXT_SIZE
+    private var textSizeValueCenter: Float = TEXT_SIZE_CENTER
+    private var radiusProgress = 0F
+    private var radiusLine = 0F
+    private var radiusText = 0F
+    private val valueData = mutableListOf<String>()
+    private val totalValue = maxValue - minValue
+    private val stepValue = totalValue / DISPLAY_LEVEL
+    private val stepDegreeText =
+        (DEGREE_END_DRAW_LINE - DEGREE_START_DRAW_LINE) * stepValue / totalValue
+    private val stepDegreeLine = stepDegreeText / 5
 
-    private val maxValue = 20F
-//     fun setMaxValue(value: Float) {
-//        this.maxValue = value
-//        invalidate()
-//    }
+    private val boundsTextValue = Rect()
+    private val boundsTextValueCenter = Rect()
+    private var centerPoint = PointF()
+    private val paintCircle: Paint = Paint()
+    private val paintProgress = Paint()
+    private val paintLine = Paint()
+    private val paintText = Paint()
 
-    private var currentValue = minValue
+    private fun initPain() {
+        paintCircle.apply {
+            isAntiAlias = true
+            strokeJoin = Paint.Join.ROUND
+            color = ContextCompat.getColor(context, R.color.green)
+            style = Paint.Style.FILL
+            shader
+        }
+
+        paintProgress.apply {
+            strokeWidth = radius * STROKE_WIDTH_PAIN_PROGRESS
+            strokeCap = Paint.Cap.ROUND
+            style = Paint.Style.STROKE
+            color = ContextCompat.getColor(context, R.color.background_progress)
+        }
+
+        paintLine.apply {
+            strokeWidth = radius * STROKE_WIDTH_PAIN_LINE
+            strokeCap = Paint.Cap.ROUND
+            style = Paint.Style.STROKE
+            color = ContextCompat.getColor(context, R.color.background_progress)
+        }
+
+        paintText.apply {
+            isAntiAlias = true
+            style = Paint.Style.FILL
+        }
+    }
+
+    private var currentValue = minValue.toFloat()
     fun setValue(value: Float) {
         if (value > minValue && value <= maxValue) {
             this.currentValue = value
@@ -42,135 +100,147 @@ class CircleTemperature : View, ValueAnimator.AnimatorUpdateListener {
         }
     }
 
-    private val bounds = Rect()
-    private var centerPoint = PointF()
-
-    private val paintCircle: Paint = Paint().apply {
-        isAntiAlias = true
-        strokeJoin = Paint.Join.ROUND
-        color = ContextCompat.getColor(context, R.color.green)
-        style = Paint.Style.FILL
-    }
-
-    private val paintBorder = Paint().apply {
-        strokeWidth = STROKE_WIDTH_PAIN_BORDER
-        strokeCap = Paint.Cap.ROUND
-        style = Paint.Style.STROKE
-        color = ContextCompat.getColor(context, R.color.green)
-    }
-
-    private val paintLine = Paint().apply {
-        strokeWidth = STROKE_WIDTH_PAIN_LINE
-        strokeCap = Paint.Cap.ROUND
-        style = Paint.Style.STROKE
-        color = ContextCompat.getColor(context, R.color.green)
-    }
-
-
-    private val paintText = Paint().apply {
-        isAntiAlias = true
-        color = ContextCompat.getColor(context, R.color.black)
-        style = Paint.Style.FILL
-        textSize = TEXT_SIZE
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        drawLine(canvas)
-        drawBorder(canvas)
-        drawCircle(canvas)
-        drawTextCenter(canvas)
-    }
-
-    private fun getRadius() =
-        if (width > height) calculateRadius(height) else calculateRadius(width)
-
-    private fun getRadiusLine() = getRadius() - MARGIN_CONTENT - LENGTH_LINE_LONG
-    private fun getRadiusBorder() = getRadiusLine() - MARGIN_CIRCLE
-    private fun getRadiusCircle() = getRadiusBorder() - MARGIN_CIRCLE
-
-    private fun calculateRadius(d: Int) = d / 2F
-
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        getCenterPoint(w, h)
+        marginContent = radius * MARGIN_CONTENT
+        spaceTextAndLine = radius * SPACE_TEXT_AND_LINE
+        spaceLineAndProgress = radius * SPACE_LINE_AND_PROGRESS
+        spaceProgressAndCircle = radius * SPACE_PROGRESS_AND_CIRCLE
+        lengthLineShort = radius * LENGTH_LINE_SHORT
+        lengthLineLong = radius * LENGTH_LINE_LONG
+        radiusProgress = radius + spaceProgressAndCircle
+        radiusLine = radiusProgress + spaceLineAndProgress
+        radiusText = radiusLine + lengthLineLong + spaceTextAndLine
+        textSizeValue = radius * TEXT_SIZE
+        textSizeValueCenter = radius * TEXT_SIZE_CENTER
+
+        rectF = RectF(
+            centerPoint.x - radiusProgress,
+            centerPoint.y - radiusProgress,
+            centerPoint.x + radiusProgress,
+            centerPoint.y + radiusProgress
+        )
+        initPain()
+    }
+
+    private fun getCenterPoint(w: Int, h: Int) {
         centerPoint.x = w / 2F
         centerPoint.y = h / 2F
     }
 
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        drawCircle(canvas)
+        drawTextCenter(canvas)
+
+        drawTextLevel(canvas)
+        drawLineFull(canvas)
+        drawProgressBorder(canvas)
+        drawLineValue(canvas)
+        drawProgressValue(canvas)
+    }
+
+    private fun drawProgressBorder(canvas: Canvas?) = drawProgress(canvas, true)
+    private fun drawProgressValue(canvas: Canvas?) = drawProgress(canvas, false)
+
+    private fun drawLineFull(canvas: Canvas?) = drawLine(canvas, true)
+    private fun drawLineValue(canvas: Canvas?) = drawLine(canvas, false)
+
+
     private fun drawCircle(canvas: Canvas?) {
-        canvas?.drawCircle(centerPoint.x, centerPoint.y, getRadiusCircle(), paintCircle)
+        canvas?.drawCircle(centerPoint.x, centerPoint.y, radius.toFloat(), paintCircle)
     }
 
     private fun drawTextCenter(canvas: Canvas?) {
-        val valueText = resources.getString(R.string.temp, currentValue.toInt())
-        paintText.reset()
-        paintText.color = ContextCompat.getColor(context, R.color.white)
-        paintText.textSize = TEXT_SIZE_CENTER
-        paintText.getTextBounds(valueText, 0, valueText.length, bounds)
-        val xPosition = centerPoint.x - bounds.width() / 2
-        val yPosition = centerPoint.y + bounds.height() / 2
+        paintText.apply {
+            reset()
+            textSize = textSizeValueCenter
+            color = ContextCompat.getColor(context, R.color.white)
+        }
+        val valueText = getStringValue(currentValue)
+        paintText.getTextBounds(valueText, 0, valueText.length, boundsTextValueCenter)
+        val xPosition = centerPoint.x - boundsTextValueCenter.width() / 2
+        val yPosition = centerPoint.y + boundsTextValueCenter.height() / 2
         canvas?.drawText(valueText, xPosition, yPosition, paintText)
     }
 
-    private fun drawBorder(canvas: Canvas?) {
-        val rectF = RectF(
-            centerPoint.x - getRadiusBorder(),
-            centerPoint.y - getRadiusBorder(),
-            centerPoint.x + getRadiusBorder(),
-            centerPoint.y + getRadiusBorder()
-        )
-        canvas?.drawArc(rectF, START_ANGLE, sweepAngle, false, paintBorder)
+    private fun drawTextLevel(canvas: Canvas?) {
+        paintText.apply {
+            reset()
+            textSize = textSizeValue
+            color = ContextCompat.getColor(context, R.color.black)
+        }
+        for ((index, degree) in (DEGREE_START_DRAW_LINE..DEGREE_END_DRAW_LINE step stepDegreeText).withIndex()) {
+            val xValue = sin(degreeToRadian(degree)).toFloat()
+            val yValue = cos(degreeToRadian(degree)).toFloat()
+            paintText.getTextBounds(valueData[index], 0, valueData[index].length, boundsTextValue)
+            val xPosition =
+                centerPoint.x + (xValue * radiusText) - boundsTextValue.width() / 2
+            val yPosition =
+                centerPoint.y + (yValue * radiusText) + boundsTextValue.height() / 2
+            canvas?.drawText(valueData[index], xPosition, yPosition, paintText)
+        }
     }
 
-    private fun drawLine(canvas: Canvas?) {
-        var value = -minValue
-        for (degree in DEGREE_START_DRAW_LINE..DEGREE_END_DRAW_LINE step getStepDegree().toInt()) {
+    private fun getStringValue(value: Float): String {
+        return if (value == value.toInt().toFloat()) resources.getString(
+            R.string.tempInt,
+            value.toInt()
+        )
+        else resources.getString(
+            R.string.tempFloat,
+            value
+        )
+    }
+
+    private fun drawProgress(canvas: Canvas?, isDrawBackground: Boolean) {
+        val startAngle = START_ANGLE
+        var sweepAngle = TOTAL_SWIPE
+        if (isDrawBackground) {
+            paintProgress.color = ContextCompat.getColor(context, R.color.background_progress)
+        } else {
+            paintProgress.color = ContextCompat.getColor(context, R.color.green)
+            sweepAngle = (currentValue - minValue) * SWEEP_ANGLE
+            if (sweepAngle == 0F) sweepAngle = 0.5F
+        }
+        canvas?.drawArc(rectF, startAngle, sweepAngle, false, paintProgress)
+    }
+
+    private fun drawLine(canvas: Canvas?, isDrawBackground: Boolean) {
+        val startDegree = DEGREE_START_DRAW_LINE
+        var endDegree = DEGREE_END_DRAW_LINE
+        if (isDrawBackground) {
+            paintLine.color = ContextCompat.getColor(context, R.color.background_progress)
+        } else {
+            paintLine.color = ContextCompat.getColor(context, R.color.green)
+            endDegree = ((currentValue.toInt() - minValue) * stepDegreeLine / 2) + startDegree
+        }
+
+        for (degree in startDegree..endDegree step stepDegreeLine) {
             val xValue = sin(degreeToRadian(degree)).toFloat()
             val yValue = cos(degreeToRadian(degree)).toFloat()
 
-
-            val startX = centerPoint.x + xValue * getRadiusLine()
-            val startY = centerPoint.y + yValue * getRadiusLine()
+            val startX = centerPoint.x - xValue * radiusLine
+            val startY = centerPoint.y + yValue * radiusLine
             var endX: Float
             var endY: Float
 
-            if (degree % 60 == 0) {
-
-                val stringValue = resources.getString(R.string.temp, value.toInt())
-                endX = centerPoint.x + xValue * (getRadiusLine() + LENGTH_LINE_LONG)
-                endY = centerPoint.y + yValue * (getRadiusLine() + LENGTH_LINE_LONG)
-
-
-                paintText.reset()
-                paintText.textSize = TEXT_SIZE
-                paintText.color = ContextCompat.getColor(context, R.color.black)
-                paintText.getTextBounds(
-                    stringValue,
-                    0,
-                    stringValue.length,
-                    bounds
-                )
-
-                //Draw text Value
-                val xPosition =
-                    centerPoint.x + (xValue * (getRadiusLine() + MARGIN_TEXT)) - (bounds.width() / 2)
-                val yPosition =
-                    centerPoint.y + (yValue * (getRadiusLine() + MARGIN_TEXT)) + (bounds.height() / 2)
-                canvas?.drawText(stringValue, xPosition, yPosition, paintText)
-                value -= getStepValue()
-
+            if (degree % DEGREE_LONG_LINE == 0) {
+                endX =
+                    centerPoint.x - xValue * (radiusLine + lengthLineLong)
+                endY = centerPoint.y + yValue * (radiusLine + lengthLineLong)
             } else {
-                endX = centerPoint.x + xValue * (getRadiusLine() + LENGTH_LINE_SHORT)
-                endY = centerPoint.y + yValue * (getRadiusLine() + LENGTH_LINE_SHORT)
+                endX = centerPoint.x - xValue * (radiusLine + lengthLineShort)
+                endY = centerPoint.y + yValue * (radiusLine + lengthLineShort)
             }
             canvas?.drawLine(startX, startY, endX, endY, paintLine)
         }
     }
 
-    private var sweepAngle = 0.5F
-
     private fun startAnimation() {
-        val animator: ValueAnimator = ValueAnimator.ofInt(minValue.toInt(), currentValue.toInt())
+        val animator: ValueAnimator = ValueAnimator.ofInt(minValue, currentValue.toInt())
         animator.duration = DELAY
         animator.addUpdateListener(this)
         animator.start()
@@ -179,39 +249,42 @@ class CircleTemperature : View, ValueAnimator.AnimatorUpdateListener {
     override fun onAnimationUpdate(p0: ValueAnimator?) {
         val valueTemp = p0?.animatedValue as? Int
         if (valueTemp != null) {
-            sweepAngle = (valueTemp + 20) * SWEEP_ANGLE
+            currentValue = valueTemp.toFloat()
         }
         invalidate()
     }
 
-    private fun getSumValue() = maxValue - minValue
-
-    private fun getStepValue() = getSumValue() / DISPLAY_LEVEL
-    private fun getStepDegree() =
-        (DEGREE_END_DRAW_LINE - DEGREE_START_DRAW_LINE) * getStepValue() / (getSumValue() * 5)
 
     private fun degreeToRadian(degree: Int) = degree * PI / 180
 
     companion object {
-        private const val LENGTH_LINE_SHORT = 30F
-        private const val LENGTH_LINE_LONG = 50F
-        private const val MARGIN_TEXT = LENGTH_LINE_LONG + 50F
+        private const val DISPLAY_LEVEL = 4
+        private const val DEFAULT_MIN_VALUE = -20
+        private const val DEFAULT_MAX_VALUE = 20
+        private const val DEFAULT_RADIUS = 250
 
+        private const val SPACE_TEXT_AND_LINE = 0.25F
+        private const val SPACE_LINE_AND_PROGRESS = 0.15F
+        private const val SPACE_PROGRESS_AND_CIRCLE = 0.15F
+
+        private const val LENGTH_LINE_SHORT = 0.15F
+        private const val LENGTH_LINE_LONG = 0.25F
+
+        private const val TEXT_SIZE = 0.2F
+        private const val TEXT_SIZE_CENTER = 0.3F
+
+        private const val DEGREE_LONG_LINE = 240 / DISPLAY_LEVEL
         private const val DEGREE_START_DRAW_LINE = 60
         private const val DEGREE_END_DRAW_LINE = 300
-        private const val MARGIN_CONTENT = 80F
-        private const val MARGIN_CIRCLE = 30F
 
+        private const val MARGIN_CONTENT = 0.1F
+
+        private const val TOTAL_SWIPE = 240F
         private const val START_ANGLE = 150F
-        private const val SWEEP_ANGLE = 240F / 40
+        private const val SWEEP_ANGLE = TOTAL_SWIPE / 40
 
-        private const val STROKE_WIDTH_PAIN_LINE = 8F
-        private const val STROKE_WIDTH_PAIN_BORDER = 30F
-
-        private const val TEXT_SIZE = 35F
-        private const val TEXT_SIZE_CENTER = 50F
-
-        private const val DISPLAY_LEVEL = 4
+        private const val STROKE_WIDTH_PAIN_LINE = 0.03F
+        private const val STROKE_WIDTH_PAIN_PROGRESS = 0.15F
 
         private const val DELAY = 1000L
     }
